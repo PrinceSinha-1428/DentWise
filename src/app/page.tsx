3ac0edac484fc7f24df6d6a24825d216c168f6a0
Prince <princesinha629@gmail.com>
2025-10-07 22:30:34 +0530

    clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
+    <h1>Home page</h1>
+    <SignUpButton mode="modal">Sign up</SignUpButton>
    </div>
   );
 }
